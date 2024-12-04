3da360f77439ff142ea3c02b1afdd56a42f00811
Erik Chou <kire5030@gmail.com>
2024-12-03 19:17:24 -0700

    fix authentication mailer test

--- test/mailers/previews/auth_mailer_preview.rb
+++
@@ -1,7 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/authentication_mailer
-class AuthenticatioPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/authentication_mailer/login_email
-  def login_email
-    AuthenticatioMailer.login_email
-  end
-end
