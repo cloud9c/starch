4869eff276dc4827a33a1ad50cd829efca25d8dc
Erik Chou <kire5030@gmail.com>
2024-12-04 06:00:43 -0700

    fix auth mailer test

--- test/mailers/authentication_mailer_test.rb
+++ test/mailers/authentication_mailer_test.rb
@@ -2,9 +2,11 @@ require "test_helper"
 
 class AuthenticationMailerTest < ActionMailer::TestCase
   test "login" do
-    user = users(:one)  # Create a fixture
-    mail = AuthenticationMailer.login_email(user, "test_token")
-    assert_equal "Log in to Starch", mail.subject
-    assert_equal [ user.email_address ], mail.to
+    user = users(:one)
+    travel_to Time.zone.local(2024, 1, 1, 12, 0, 0) do
+      mail = AuthenticationMailer.login_email(user, "test_token")
+      assert_equal "Secure link to log in to Starch | 2024-01-01 12:00:00", mail.subject
+      assert_equal [user.email_address], mail.to
+    end
   end
 end
