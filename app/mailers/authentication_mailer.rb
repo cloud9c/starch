95dcfedc43d5e4f35d60c30be4f63fe0a5505857
Erik Chou <kire5030@gmail.com>
2024-12-04 05:57:48 -0700

    change auth email format

--- app/mailers/authentication_mailer.rb
+++ app/mailers/authentication_mailer.rb
@@ -2,6 +2,6 @@ class AuthenticationMailer < ApplicationMailer
   def login_email(user, token)
     @user = user
     @url = magic_link_session_url(token: token)
-    mail to: user.email_address, subject: "Log in to Starch"
+    mail to: user.email_address, subject: "Secure link to log in to Starch | #{Time.current.strftime('%Y-%m-%d %H:%M:%S')}"
   end
 end
