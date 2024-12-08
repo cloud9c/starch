095595c266f6db2981ee0e9d20e4135e1cb61224
cloud9c <kire5030@gmail.com>
2024-12-08 10:18:14 -0700

    rename landing to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
-class LandingController < ApplicationController
+class HomeController < ApplicationController
   allow_unauthenticated_access
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root "landing#index"
+  root "home#index"
 
   resource :session do
     get :magic_link
