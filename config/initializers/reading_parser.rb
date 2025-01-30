843b4c3af17d39903361d2d957cdabcb4980925e
Erik Chou <kire5030@gmail.com>
2025-01-30 14:36:23 -0700

    rubocop

--- config/initializers/reading_parser.rb
+++ config/initializers/reading_parser.rb
@@ -1,7 +1,7 @@
 module ReadingParser
   def self.parse(html)
     uri = "http://#{Rails.env.production? ? 'starch-reading_parser' : 'localhost'}:3001/parse"
-    headers = { 'Content-Type' => 'application/json' }
+    headers = { "Content-Type" => "application/json" }
     body = { html: html }.to_json
 
     begin
