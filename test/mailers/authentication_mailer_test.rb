fc7f45d96246ac81d724b6dffdb5a9d168b6dde3
Erik Chou <kire5030@gmail.com>
2025-01-30 14:40:05 -0700

    fix test case

--- test/mailers/authentication_mailer_test.rb
+++ test/mailers/authentication_mailer_test.rb
@@ -5,7 +5,7 @@ class AuthenticationMailerTest < ActionMailer::TestCase
     user = users(:one)
     travel_to Time.zone.local(2024, 1, 1, 12, 0, 0) do
       mail = AuthenticationMailer.login_email(user, "test_token", 123123)
-      assert_equal "Secure link to log in to Starch | 2024-01-01 12:00:00", mail.subject
+      assert_equal "Log in to Starch (123123)", mail.subject
       assert_equal [ user.email_address ], mail.to
     end
   end
