d1b0be576c3876119fc873a6fd28b505d7e414dd
Erik Chou <kire5030@gmail.com>
2025-03-04 19:46:11 -0700

    rubocop -a

--- app/helpers/http_helper.rb
+++ app/helpers/http_helper.rb
@@ -93,23 +93,23 @@ module HttpHelper
     host = normalize_url(URI(url).host)
     response = get(host)
     return unless response
-    
+
     doc = Nokogiri::HTML(body_to_s(response))
     candidates = doc.css('link[rel~="icon"], link[rel~="apple-touch-icon"]').map { |link| link[:href] }.compact
-    
+
     absolute_candidates = candidates.map { |href| get_absolute_url(href, host) }.compact
-    
+
     ranked_images = absolute_candidates.map do |abs_url|
       size = FastImage.size(abs_url)
-      [abs_url, size] if size
+      [ abs_url, size ] if size
     end.compact
-    
+
     ranked_by_area = ranked_images.map do |abs_url, size|
-      [abs_url, size[0] * size[1]]
+      [ abs_url, size[0] * size[1] ]
     end
-    
+
     largest_image = ranked_by_area.sort_by { |_, area| -area }.first
-    
+
     largest_image ? largest_image[0] : nil
   end
 
