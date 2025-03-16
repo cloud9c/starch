71a51e30c33103bf62f475877a79b59c17f54e8a
Erik Chou <kire5030@gmail.com>
2025-03-15 21:48:59 -0700

    fix argument expectation

--- app/jobs/update_channels_job.rb
+++ app/jobs/update_channels_job.rb
@@ -3,7 +3,7 @@ class UpdateChannelsJob < ApplicationJob
     Channel.find_each do |channel|
       delay = rand(0..12).hours.to_i
 
-      UpdateChannelJob.set(wait: delay).perform_later(channel.id)
+      UpdateChannelJob.set(wait: delay).perform_later(channel.id, false)
     end
   end
 end
