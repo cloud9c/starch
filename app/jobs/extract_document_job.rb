5b76f55ae5daa8e38aa9d3e8b4873516738ed92d
Erik Chou <kire5030@gmail.com>
2025-03-17 13:33:44 -0700

    bugs

--- app/jobs/extract_document_job.rb
+++ app/jobs/extract_document_job.rb
@@ -1,6 +1,6 @@
 class ExtractDocumentJob < ApplicationJob
   def perform(document_id)
-    document = Document.find!(document_id)
+    document = Document.find(document_id)
     document.extracted_data
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
   has_many :verifications, dependent: :destroy
 
-  has_many :folders, dependent: :destroy
+  has_many :tags, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
   has_many :channels, through: :subscriptions
   has_many :document_states, dependent: :destroy
