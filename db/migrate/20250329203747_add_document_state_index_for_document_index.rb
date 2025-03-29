ad003b39e9fb0525f765cf379342fda10d501aa9
cloud9c <kire5030@gmail.com>
2025-03-29 15:31:24 -0600

    russian doll caching + correct appending

--- db/migrate/20250329203747_add_document_state_index_for_document_index.rb
+++ db/migrate/20250329203747_add_document_state_index_for_document_index.rb
@@ -1,5 +1,5 @@
 class AddDocumentStateIndexForDocumentIndex < ActiveRecord::Migration[8.0]
   def change
-    add_index :document_states, [:user_id, :status, :read]
+    add_index :document_states, [ :user_id, :status, :read ]
   end
 end
