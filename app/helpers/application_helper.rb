e288c36768377af9f55a0bb58a8604ee72ac23b9
Erik Chou <kire5030@gmail.com>
2025-04-04 19:12:22 -0400

    show youtube iframe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,4 +23,25 @@ module ApplicationHelper
         vspace itemprop id
       ])
   end
+
+  def render_video(document)
+    youtube_regex = /^(?:https?:\/\/)?(?:(?:www\.)?youtube.com\/watch\?v=|youtu.be\/)(\w+)$/
+
+    match = youtube_regex.match(document.url)
+
+    return unless match
+
+    youtube_id = match[1]
+
+    content_tag :div, class: 'video-container' do
+      content_tag :iframe, nil, 
+                  src: "https://www.youtube.com/embed/#{youtube_id}", 
+                  width: "100%", 
+                  height: "100%", 
+                  frameborder: 0, 
+                  allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture", 
+                  allowfullscreen: true,
+                  title: document.title
+    end
+  end
 end
