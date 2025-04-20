e3af424a34820b7587860c11aa10ef1917f1a051
cloud9c <kire5030@gmail.com>
2025-04-20 14:30:55 -0600

    dont use bang

--- app/jobs/update_channel_job.rb
+++ app/jobs/update_channel_job.rb
@@ -1,6 +1,6 @@
 class UpdateChannelJob < ApplicationJob
   def perform(channel_id)
-    channel = Channel.find!(channel_id)
+    channel = Channel.find(channel_id)
 
     if channel.update_feed_content
       UpdateChannelMetadataJob.perform_later(channel.id)
