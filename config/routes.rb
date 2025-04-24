2ed236eb78b91c32e14fba2e726e0aca2adf6299
Erik Chou <kire5030@gmail.com>
2025-04-23 20:29:43 -0600

    lint

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resource :session do
     post :verify
   end
-  resolve("Session") { [:session] }
+  resolve("Session") { [ :session ] }
 
   resources :subscriptions
 
