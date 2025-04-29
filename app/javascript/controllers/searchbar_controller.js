d2a7dac0f44691dfb2d3c870a741148c3cec2f83
Erik Chou <kire5030@gmail.com>
2025-04-29 16:16:05 -0600

    update url on search

--- app/javascript/controllers/searchbar_controller.js
+++ app/javascript/controllers/searchbar_controller.js
@@ -3,18 +3,26 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     this.element.addEventListener("keydown", this.handleKeydown.bind(this))
+    this.form = this.element.closest("form")
+    
+    if (this.form) {
+      this.form.addEventListener("turbo:submit-end", this.updateUrl.bind(this))
+    }
   }
 
   disconnect() {
     this.element.removeEventListener("keydown", this.handleKeydown.bind(this))
+    
+    if (this.form) {
+      this.form.removeEventListener("turbo:submit-end", this.updateUrl.bind(this))
+    }
   }
-
  
   handleKeydown(event) {
     switch (event.key) {
       case "Enter":
         if (this.element.value.trim() !== "") {
-          this.element.closest("form").requestSubmit()
+          this.form.requestSubmit()
         } else {
           event.preventDefault()
         }
@@ -26,6 +34,16 @@ export default class extends Controller {
         if (event.key === "Backspace" && !event.metaKey) break;
         this.element.value = ""
         break;
+    }
+  }
+
+  updateUrl() {
+    const searchValue = this.element.value.trim()
+    if (searchValue !== "") {
+      const url = new URL(window.location)
+      url.searchParams.set('q', searchValue)
+      
+      window.history.replaceState({ q: searchValue }, '', url.toString())
     }
   }
 }
