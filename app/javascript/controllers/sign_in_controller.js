866d1eb21936f6a5a156b1f062ee23899ebdf610
Erik Chou <kire5030@gmail.com>
2025-05-01 10:21:40 -0600

    keep modal for /session/code + re-popup modal when dismissed

--- app/javascript/controllers/sign_in_controller.js
+++ app/javascript/controllers/sign_in_controller.js
@@ -3,6 +3,16 @@ import { FetchRequest } from '@rails/request.js'
 
 export default class extends Controller {
   connect() {
+    this.handleFocus();
+    
+    window.addEventListener('focus', this.handleFocus.bind(this))
+  }
+  
+  disconnect() {
+    window.removeEventListener('focus', this.handleFocus.bind(this))
+  }
+  
+  handleFocus() {
     Turbo.visit("/session/new")
   }
 }
