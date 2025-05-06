a4d705ab2a5b96a2a6ba969521d5122c022a148c
Erik Chou <kire5030@gmail.com>
2025-05-05 19:19:32 -0600

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   before_action :redirect_www_to_non_www
 
   def redirect_www_to_non_www
-    if request.host.start_with?('www.')
-      non_www_host = request.host.sub('www.', '')
+    if request.host.start_with?("www.")
+      non_www_host = request.host.sub("www.", "")
       redirect_url = "#{request.protocol}#{non_www_host}#{request.fullpath}"
       redirect_to redirect_url, status: :moved_permanently, allow_other_host: true
     end
