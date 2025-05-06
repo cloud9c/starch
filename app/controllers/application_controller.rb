b77d42ac8d08e4e02cef435ad7cfb0ec5e72520f
Erik Chou <kire5030@gmail.com>
2025-05-05 18:20:32 -0600

    redirect www to non www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,12 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :redirect_www_to_non_www
   private
+  
+  def redirect_www_to_root
+    if request.host.start_with?('www.')
+      redirect_to request.original_url.sub('www.', ''), status: :moved_permanently
+    end
+  end
 end
