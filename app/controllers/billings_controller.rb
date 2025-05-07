700da30582997619ae6716af77f343410429596f
Erik Chou <kire5030@gmail.com>
2025-05-06 23:55:21 -0600

    enable promotion codes

--- app/controllers/billings_controller.rb
+++ app/controllers/billings_controller.rb
@@ -52,7 +52,8 @@ class BillingsController < ApplicationController
       mode: "subscription",
       success_url: "#{request.base_url}/user/billing/return?session_id={CHECKOUT_SESSION_ID}",
       cancel_url: "#{request.base_url}/user",
-      automatic_tax: { enabled: true }
+      automatic_tax: { enabled: true },
+      allow_promotion_codes: true,
     }
 
     if Current.user.stripe_customer_id.present?
