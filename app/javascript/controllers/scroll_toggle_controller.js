5b2fb1969d4e772ae66ada363caf1ff631a4f1f7
Erik Chou <kire5030@gmail.com>
2025-05-11 14:30:25 -0600

    use opacity to hide toolbar on scrolldown

--- app/javascript/controllers/scroll_toggle_controller.js
+++ app/javascript/controllers/scroll_toggle_controller.js
@@ -14,19 +14,27 @@ export default class extends Controller {
   
   handleScroll() {
     const currentScrollTop = window.scrollY || document.documentElement.scrollTop
+    const scrollHeight = document.documentElement.scrollHeight
+    const clientHeight = document.documentElement.clientHeight
     
-    // Handle body--scrolled class
     if (currentScrollTop > 0) {
       document.body.classList.add("body--scrolled")
     } else {
       document.body.classList.remove("body--scrolled")
     }
     
-    // Handle body--scrolled-down class
     if (currentScrollTop > this.lastScrollTop) {
       document.body.classList.add("body--scrolled-down")
-    } else {
+      document.body.classList.remove("body--scrolled-up")
+    } else if (currentScrollTop < this.lastScrollTop) {
       document.body.classList.remove("body--scrolled-down")
+      document.body.classList.add("body--scrolled-up")
+    }
+    
+    if (Math.abs(scrollHeight - clientHeight - currentScrollTop) < 1) {
+      document.body.classList.add("body--scrolled-bottom")
+    } else {
+      document.body.classList.remove("body--scrolled-bottom")
     }
     
     this.lastScrollTop = currentScrollTop
