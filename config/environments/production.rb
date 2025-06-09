56623a08efafa32dcd4377009e941ff4650ccea3
Erik Chou <kire5030@gmail.com>
2025-06-09 13:35:26 -0600

    redeploy with www.starch.app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,8 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    "starch.app"
+    "starch.app",
+    "www.starch.app"
   ]
 
   # Skip DNS rebinding protection for the default health check endpoint.
